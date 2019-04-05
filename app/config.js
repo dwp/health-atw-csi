5615c070dd0938d1df24c527df9aa5fd0b9d67c3
a.valentine <andrew.valentine@engineering.digital.dwp.gov.uk>
2019-04-05 09:24:30 +0100

    chore: first V7 commit

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'CSI Prototype',
+  serviceName: 'Access To Work',
 
   // Default port that prototype runs on
   port: '3000',
