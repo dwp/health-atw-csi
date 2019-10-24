f7e336351a7201df30341862aab298b2d6ba86e5
Vicky Teinaki <vicky.teinaki@dwp.gov.uk>
2019-10-24 13:22:28 +0100

    new version of prototype based on advisers

--- app/routes.js
+++ app/routes.js
@@ -32,5 +32,5 @@ router.use('/', (req, res, next) => {
 router.use('/V12', require('./views/V12/_routes'));
 router.use('/baseline-as-is', require('./views/baseline-as-is/_routes'));
 router.use('/sprint-4', require('./views/sprint-4/_routes'));
-
+router.use('/sprint-4a', require('./views/sprint-4a/_routes'));
 module.exports = router
