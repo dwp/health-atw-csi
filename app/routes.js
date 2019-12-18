b6ca0dd0fa3b6467d85df42bca8e0b12b7e7c7d4
Vicky Teinaki <vicky.teinaki@dwp.gov.uk>
2019-12-18 14:47:45 +0000

    starting point for new sprint

--- app/routes.js
+++ app/routes.js
@@ -35,6 +35,7 @@ router.use('/sprint-4', require('./views/sprint-4/_routes'));
 router.use('/sprint-4a', require('./views/sprint-4a/_routes'));
 router.use('/sprint-5', require('./views/sprint-5/_routes'));
 router.use('/sprint-6', require('./views/sprint-6/_routes'));
+router.use('/sprint-7', require('./views/sprint-7/_routes'));
 // current sprint, remember to add older sprint when adding a new folder!
 router.use('/current', require('./views/current/_routes'));
 module.exports = router
